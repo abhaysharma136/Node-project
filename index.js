e7e89e1da095ee9b21f768a97ef33ddea92522e2
Abhay Sharma <abhaysharmajr@gmail.com>
2023-01-24 13:58:47 +0530

    quickSort alogorithm

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 import { userData1, userData2 } from "./data.js";
+
+function SortArr(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  let pivot = arr[arr.length - 1];
+  // return pivot;
+  let left = [];
+  let right = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i].Age < pivot.Age) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return [...SortArr(left), pivot, ...SortArr(right)];
+}
+
 function getUsersInState(data, state1) {
   let filterArr = data.filter((ele) => ele.state === state1);
-  console.log(filterArr.sort((a, b) => a.age - b.age));
+  console.log(SortArr(filterArr));
   let initialValue = 0;
   let AverageAge = filterArr.reduce(
     (sum, curr) => sum + curr.age,
